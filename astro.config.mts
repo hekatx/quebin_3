3deaf435f1b6254a5f98dd20492f309f61ddb22c
hekate <phyzheye01@gmail.com>
2024-03-16 14:20:43 -0600

    good work

--- astro.config.mts
+++ astro.config.mts
@@ -8,6 +8,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), react(), tailwind()],
+  site: 'https://example.com',
+  integrations: [mdx({ rehypePlugins }), sitemap(), react(), tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			keyframes: {
-				appear: {
-					'0%': { opacity: 0, transform: 'translateY(10px)' },
-					'100%': { opacity: 1, transform: 'none' },
-				},
-			},
-			animation: {
-				appear: 'appear 0.6s both calc(var(--stagger, 0) * 120ms)',
-			},
-		},
-	},
-	safelist: ['sr-only'],
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        appear: {
+          '0%': { opacity: 0, transform: 'translateY(10px)' },
+          '100%': { opacity: 1, transform: 'none' },
+        },
+      },
+      animation: {
+        appear: 'appear 0.6s both calc(var(--stagger, 0) * 120ms)',
+      },
+      maxWidth: {
+        container: '43rem',
+      },
+    },
+  },
+  safelist: ['sr-only'],
+  plugins: [],
 };
